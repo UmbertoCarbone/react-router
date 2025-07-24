fc1a1c8b224c0d28ec2b53448c8d2044d4c76555
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-07-24 18:51:39 +0200

    Aggiungi funzionalità per visualizzare i prodotti dalla API

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,9 +1,32 @@
+import { useState, useEffect } from "react";
+
 export default function ProductsPage() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products")
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
+
     return (
         <>
             <main>
                 <h1>this is a Products page</h1>
                 <p>Prova</p>
+                <div className="row">
+                    {products.map(product => (
+                        <div className="col-md-4 mb-4" key={product.id}>
+                            <div className="card h-100">
+                                <img src={product.image} className="card-img-top p-3" alt={product.title} style={{ height: "200px", objectFit: "contain" }} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{product.title}</h5>
+                                    <p className="card-text">{product.price} €</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </main>
         </>
     )
